b35c360672949b7ffdd37f8c151f3b0228c5385f
Martin <wryxo@localhost.localdomain>
2015-01-05 14:06:47 +0100

    Posledne zmeny
    Signed-off-by: Martin <wryxo@localhost.localdomain>

--- siii/src/library.cpp
+++ siii/src/library.cpp
@@ -5,24 +5,22 @@
 
 using namespace std;
 
-#define LL long
-
 template <class T>
 class Range {
     T start;
     T end;
     T jump;
 public:
-    T at(LL i) {
+    T at(long i) {
         return start + i * jump;
     }
 
-    bool last(LL i) {
+    bool last(long i) {
         return i >= end;
     }
 
-    LL size() {
-        return ((LL)((end - start)/jump));
+    long size() {
+        return ((long)((end - start)/jump));
     }
 
     Range(T s, T e, T j) {
@@ -33,7 +31,7 @@ public:
 };
 
 template <class T>
-LL print(T v) {
+long print(T v) {
     cout << v << endl;
     return 0;
 }
@@ -46,7 +44,7 @@ T read() {
 }
 
 template <class T>
-vector<T>* createList(LL size) {
+vector<T>* createList(long size) {
     vector<T>* v = new vector<T>(size);
     // cout << "List:" << v << endl;
     return v;
@@ -62,14 +60,14 @@ vector<T>* createListFromRange(Range<T>* range) {
 }
 
 template <class T>
-T getListItem(vector<T> *list, LL index) {
+T getListItem(vector<T> *list, long index) {
     T item = (*list)[index];
     // cout << "Get:" << item << endl;
     return item;
 }
 
 template <class T>
-void setListItem(vector<T> *list, LL index, T item) {
+void setListItem(vector<T> *list, long index, T item) {
     (*list)[index] = item;
     //cout << "Set:" << (*list)[index] << endl;
 }
@@ -81,79 +79,53 @@ extern "C"
 #endif
 
 //--Int------------------
-LL printInt(LL a) {
+long printInt(long a) {
     return print(a);
 }
 
-LL readInt() {
-    return read<LL>();
+long readInt() {
+    return read<long>();
 }
 
-void* createListInt(LL size) {
-    return (void*) createList<LL>(size);
+void* createListInt(long size) {
+    return (void*) createList<long>(size);
 }
 
 void* createListFromRangeInt(void* range) {
-    return (void*) createListFromRange<LL>((Range<LL>*) range);
+    return (void*) createListFromRange<long>((Range<long>*) range);
 }
 
-void* createRangeInt(LL start, LL end, LL jump) {
-    return (void*) new Range<LL>(start, end, jump);
+void* createRangeInt(long start, long end, long jump) {
+    return (void*) new Range<long>(start, end, jump);
 }
 
-void setListItemInt(void* list, LL index, LL item) {
-    setListItem<LL>((vector<LL>*) list, index, item);
+void setListItemInt(void* list, long index, long item) {
+    setListItem<long>((vector<long>*) list, index, item);
 }
 
-LL getListItemInt(void* list, LL index) {
-    getListItem<LL>((vector<LL>*) list, index);
+long getListItemInt(void* list, long index) {
+    getListItem<long>((vector<long>*) list, index);
 }
 
-LL getRangeItemInt(void* range, LL index) {
-    return ((Range<LL>*) range)->at(index);
+long getRangeItemInt(void* range, long index) {
+    return ((Range<long>*) range)->at(index);
 }
 
-LL sizeListInt(void* list1) {
-    return ((vector<LL> *)list1)->size();
+long sizeListInt(void* list1) {
+    return ((vector<long> *)list1)->size();
 }
 
-LL sizeRangeInt(void* range1) {
-    return ((Range<LL> *)range1)->size();
-}
-
-//--Bool------------------
-
-LL printBool(bool a) {
-    return print(a);
-}
-
-void* createListBool(LL size) {
-    return (void*) createList<bool>(size);
-}
-
-void* createListFromRangeBool(void* range) {
-    return (void*) createListFromRange<bool>((Range<bool>*) range);
-}
-
-void setListItemBool(void* list, LL index, bool item) {
-    setListItem<bool>((vector<bool>*) list, index, item);
-}
-
-bool getListItemBool(void* list, LL index) {
-    getListItem<bool>((vector<bool>*) list, index);
-}
-
-LL sizeListBool(void* list1) {
-    return ((vector<bool> *)list1)->size();
+long sizeRangeInt(void* range1) {
+    return ((Range<long> *)range1)->size();
 }
 
 //--Char------------------
 
-LL printChar(char a) {
+long printChar(char a) {
     return print(a);
 }
 
-LL printString(vector<char>* list) {
+long printString(vector<char>* list) {
     for (int i = 0; i < list->size(); i++) {
         cout << (*list)[i];
     }
@@ -164,7 +136,7 @@ void* createRangeChar(char start, char end, char jump) {
     return (void*) new Range<char>(start, end, jump);
 }
 
-void* createListChar(LL size) {
+void* createListChar(long size) {
     return (void*) createList<char>(size);
 }
 
@@ -172,35 +144,16 @@ void* createListFromRangeChar(void* range) {
     return (void*) createListFromRange<char>((Range<char>*) range);
 }
 
-void setListItemChar(void* list, LL index, char item) {
+void setListItemChar(void* list, long index, char item) {
     setListItem<char>((vector<char>*) list, index, item);
 }
 
-char getListItemChar(void* list, LL index) {
+char getListItemChar(void* list, long index) {
     getListItem<char>((vector<char>*) list, index);
 }
 
-LL sizeListChar(void* list1) {
+long sizeListChar(void* list1) {
     return ((vector<char> *)list1)->size();
-}
-
-
-//--List----------------------
-
-void* createListPointer(LL size) {
-    return (void*) createList<void*>(size);
-}
-
-void setListItemPointer(void* list, LL index, void* item) {
-    setListItem<void*>((vector<void*>*) list, index, item);
-}
-
-void* getListItemPointer(void* list, LL index) {
-    getListItem<void*>((vector<void*>*) list, index);
-}
-
-LL sizeListPointer(void* list1) {
-    return ((vector<void*> *)list1)->size();
 }
 
 #ifdef __cplusplus
